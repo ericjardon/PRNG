725d73c27685419eac69c3006fac585101eae083
unknown <a01026848@itesm.mx>
2022-03-29 14:17:36 -0600

    Finished midSquares

--- src/stats/methods.ts
+++ src/stats/methods.ts
@@ -1,6 +1,6 @@
 import randomDummy from "./dummy"
-
-
+// import all the methods to be used.
+import randomMidSquares from './midSquares'
 
 const randomNotImplemented = (params:any) => {
     // midSquares and all others should be imported from their respective module.
@@ -11,7 +11,7 @@ type RandomGeneratorFunc = (seed:number, params: any) => number;
 
 export const METHODS : Record<string,RandomGeneratorFunc> = {
     'dummy': randomDummy,
-    'midSquares': randomNotImplemented,
+    'midSquares': randomMidSquares,
     'MC': randomNotImplemented,
     'MCM': randomNotImplemented,
 }
