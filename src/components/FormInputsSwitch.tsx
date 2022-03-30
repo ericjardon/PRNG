4e3c9facd0e337d9472ebd3f199ab4d1ee0a82dc
eric chao <ericjardon@hotmail.com>
2022-03-30 00:20:54 -0600

    progress on MCM form component. Reusable for MC simple component

--- src/components/FormInputsSwitch.tsx
+++ src/components/FormInputsSwitch.tsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import MidSquares from './MidSquares';
+import MCM from './MCM';
 // importar todos los inputs específicos a métodos
 
 export interface IFormInputsSwitchProps {
     method: string,
-    updateHandler: (event:React.FormEvent<HTMLInputElement>) => void
+    updateHandler: (event: React.ChangeEvent<any>) => void //(event:React.FormEvent<HTMLInputElement>) => void
 }
 
-export default function FormInputsSwitch (props: IFormInputsSwitchProps) {
+const FormInputsSwitch : React.FC<IFormInputsSwitchProps> = ({
+  method,
+  updateHandler,
+}) => {
 
 
-  if (props.method==='midSquares') {
+  if (method==='midSquares') {
     return (
       <MidSquares/>
+    )
+  }
+
+  if (method==='MCM' || method==='MC') {
+    return (
+      <MCM updateHandler={updateHandler}/>
     )
   }
 
@@ -21,3 +31,5 @@ export default function FormInputsSwitch (props: IFormInputsSwitchProps) {
     </>
   );
 }
+
+export default FormInputsSwitch
---
+++ src/components/MCM.tsx
@@ -0,0 +1,35 @@
+
+import React, { ChangeEventHandler, useState } from 'react';
+import { Button, TextField, Stack, Select, MenuItem, InputLabel } from '@mui/material'
+import { SelectChangeEvent } from '@mui/material/Select';
+/* a: number, // multiplicador
+c: number, // increment
+m: number, // module
+ */
+interface Props {
+    a?:number,
+    c?:number,
+    m?:number,
+    updateHandler: Handler,
+}
+
+type Handler = (event: React.ChangeEvent<any>) => void;
+
+const MCM : React.FC<Props> = ({
+    a,
+    c,
+    m,
+    updateHandler
+}) => {
+
+
+    return (
+        <>
+        <TextField name="m" label="Módulo" variant="filled" value={m || ''} onChange={updateHandler}/>
+        <TextField name="a" label="Multiplicador" variant="filled" value={a || ''} onChange={updateHandler}/>
+        <TextField name="c" label="Incremento" variant="filled" value={c || ''} onChange={updateHandler}/>
+        </>
+    )
+}
+
+export default MCM;
