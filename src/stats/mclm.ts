e094152105dde79e325f0d728a6aa100ba8aa598
Georgina <gina10111@hotmail.com>
2022-04-12 22:22:16 -0500

    change mclm to add seed for generator and change the array to set

--- src/stats/mclm.ts
+++ src/stats/mclm.ts
@@ -1,38 +1,36 @@
 interface MCLMParams {
     a: number[],
     m:number[],
+    xi: number[],
     iterations: number
 }
 
 
 const randomMCLM = (seed:number, params: MCLMParams) => {
-    let {a,m,iterations} = params;
-    let result = new Set<number>();
+    let {a,m, xi, iterations} = params;
+    let result = new Array<number>();
     let generators = Array(iterations).fill(Array(a.length).fill(0));
-    let temseed, r : number | undefined;
-    function get_gm(a_single: number, m_single: number, temseed: number) {
-        return (a_single*temseed) % m_single;
+    let temseed, r : number;
+    function get_gm(a_single: number, m_single: number, xi_single: number) {
+        return (a_single*xi_single) % m_single;
     }
     
     for(let j = 0; j < a.length ; j++ ){
-        temseed = seed
         for(let i = 0; i<iterations; i++){
-            temseed = get_gm(a[j], m[j], temseed)
+            xi[j] = get_gm(a[j], m[j], xi[j])
             generators[i][j] = temseed
         }
     }
 
-    for(let j = 0; j < a.length ; j++ ){
-        for(let i = 0; i< iterations; i++){
+    for(let i = 0; i< iterations; i++){
+        r = 0
+        for(let j = 0; j < a.length ; j++ ){
             r += generators[i][j]
-            if(result.has(r as number)){
-                break
-            }
-            result.add(r as number)
         }
+        result.push(r)
     }
     console.log(result)
-    return Array.from( result );
+    return  result ;
     
 
 
